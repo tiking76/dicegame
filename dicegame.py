7110b21dbb19fc2caf88dff234afad8ddad506b9
tiking76 <716yosiki1999@gmail.com>
2019-07-11 23:44:43 +0900

    first commit

---
+++ dicegame.py
@@ -0,0 +1,10 @@
+import random
+a = random.randint(1,6)
+b = random.randint(1,6)
+print("Rolling the dice...")
+print("Die 1: " + str(a))
+print("Die 2: " + str(b))
+c = 0
+c = a + b
+print("Total value: " + str(c))
+
