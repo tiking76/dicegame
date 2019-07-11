53ded54c09e2bcb3c91a0b914d43ca3211665fbf
tiking76 <716yosiki1999@gmail.com>
2019-07-11 23:59:42 +0900

    last commit

--- dicegame.py
+++ dicegame.py
@@ -1,11 +1,15 @@
 import random
 a = random.randint(1,6)
 b = random.randint(1,6)
+d = input("What is your name ?  ")
+print("Hello, " + str(d) + "!")
 print("Rolling the dice...")
 print("Die 1: " + str(a))
 print("Die 2: " + str(b))
 c = 0
 c = a + b
 print("Total value: " + str(c))
-d = input("What is your name ?")
-
+if c > 7:
+    print(str(d) + " won!")
+else:
+    print(str(d) + " lost!")
